4be0842726d57594a9344716201ac3e352ad71fa
236438 <236438@edu.p.lodz.pl>
2023-10-22 14:09:21 +0200

    rename Command Manager

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestRectangle2OptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestRectangle2OptionListener.java
@@ -2,8 +2,7 @@ package edu.kis.powp.jobs2d.events;
 
 import edu.kis.powp.jobs2d.command.ComplexCommandFactory;
 import edu.kis.powp.jobs2d.command.DriverCommand;
-import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
-import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.command.manager.CommandManager;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
 
 import java.awt.event.ActionEvent;
@@ -17,7 +16,7 @@ public class SelectTestRectangle2OptionListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         List<DriverCommand> commands = new ArrayList<DriverCommand>();
         commands.add(ComplexCommandFactory.getRectangle());
-        DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
+        CommandManager manager = CommandsFeature.getDriverCommandManager();
         manager.setCurrentCommand(commands, "getRectangle");
     }
 }
