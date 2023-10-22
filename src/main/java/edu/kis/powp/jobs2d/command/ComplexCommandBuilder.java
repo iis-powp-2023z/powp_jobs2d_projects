6df2480092be112c2201c395bc87906c1faa036e
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-22 13:20:08 +0200

    Utworzenie buildera

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandBuilder.java
@@ -0,0 +1,15 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommandBuilder {
+    private final List<DriverCommand> complexCommandList = new ArrayList<>();
+    public ComplexCommandBuilder addCommand(DriverCommand driverCommand) {
+        complexCommandList.add(driverCommand);
+        return this;
+    }
+    public ComplexCommand build() {
+        return new ComplexCommand(complexCommandList);
+    }
+}
