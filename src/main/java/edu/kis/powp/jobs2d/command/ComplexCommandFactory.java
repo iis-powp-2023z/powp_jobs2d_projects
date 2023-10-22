6e9c34b21b82a3d5ac5a9d9195afd736e0b70b7c
Janusz Wdowka <j.wdowka2000@gmail.com>
2023-10-22 13:28:57 +0200

    factory fix

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandFactory.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandFactory.java
@@ -3,7 +3,7 @@ package edu.kis.powp.jobs2d.command;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class ComplexCommandFactory {
-    public static DriverCommand getRectangle(Job2dDriver job2dDriver) {
+    public static DriverCommand getRectangle() {
         return new ComplexCommandBuilder()
                 .addCommand(new SetPositionCommand(-100, -50))
                 .addCommand(new OperateToCommand(100, -50))
@@ -12,7 +12,7 @@ public class ComplexCommandFactory {
                 .addCommand(new OperateToCommand(-100, -50))
                 .build();
     }
-    public static DriverCommand getTriangle(Job2dDriver job2dDriver) {
+    public static DriverCommand getTriangle() {
         return new ComplexCommandBuilder()
                 .addCommand(new SetPositionCommand(-100, -100))
                 .addCommand(new OperateToCommand(-100, 100))
