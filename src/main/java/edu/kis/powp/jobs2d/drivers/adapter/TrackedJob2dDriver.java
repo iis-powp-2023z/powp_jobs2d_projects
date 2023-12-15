9c611d3b6af3bb8ab31c842513b448274a23f28f
Adam Ludwiczak <64928495+vAdamski@users.noreply.github.com>
2023-12-15 21:43:48 +0100

    Implemented saving previous position to calculate distance

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/TrackedJob2dDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/TrackedJob2dDriver.java
@@ -9,6 +9,9 @@ public class TrackedJob2dDriver implements Job2dDriver {
     private final Job2dDriver delegate;
     private final IUsageMonitor usageMonitor;
 
+    private double startX = 0;
+    private double startY = 0;
+
     public TrackedJob2dDriver(Job2dDriver delegate, IUsageMonitor usageMonitor) {
         this.delegate = delegate;
         this.usageMonitor = usageMonitor;
@@ -17,15 +20,22 @@ public class TrackedJob2dDriver implements Job2dDriver {
     @Override
     public void setPosition(int x, int y) {
         delegate.setPosition(x, y);
-        usageMonitor.addFullDistanceTraveled(0, 0, x, y);
+        usageMonitor.addFullDistanceTraveled(startX, startY, x, y);
         usageMonitor.printUsage();
+        updatePosition(x, y);
     }
 
     @Override
     public void operateTo(int x, int y) {
         delegate.operateTo(x, y);
-        usageMonitor.addWorkDistanceTraveled(0, 0, x, y);
-        usageMonitor.addFullDistanceTraveled(0, 0, x, y);
+        usageMonitor.addWorkDistanceTraveled(startX, startY, x, y);
+        usageMonitor.addFullDistanceTraveled(startX, startY, x, y);
         usageMonitor.printUsage();
+        updatePosition(x, y);
+    }
+
+    private void updatePosition(int x, int y) {
+        startX = x;
+        startY = y;
     }
 }
